3bb74cb363401a5cfe202ab3dee08d520309b9ea
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-02-13 13:27:40 +0100

    Finish unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,18 +7,47 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setClicks] = useState(0)
+
+  const increaseGood = () => {
+    setGood(good + 1)
+    setClicks(allClicks + 1)
+  }
+
+  const increaseNeutral = () => {
+    setNeutral(neutral + 1)
+    setClicks(allClicks + 1)
+  }
+
+  const increaseBad = () => {
+    setBad(bad + 1)
+    setClicks(allClicks + 1)
+  }
+
+  const calculateAverage = () => {
+    const score = good - bad
+    return score / allClicks || 0
+  }
+
+  const calculatePositivePercentage = () => {
+    
+    return (good / allClicks) * 100 || 0
+  }
 
   return (
     <div>
       <h2>give feedback</h2>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={increaseGood}>good</button>
+      <button onClick={increaseNeutral}>neutral</button>
+      <button onClick={increaseBad}>bad</button>
       
       <h2>statistics</h2>
-      <div>good: {good}</div>
-      <div>neutral: {neutral}</div>
-      <div>bad: {bad}</div>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {allClicks}</div>
+      <div>average {calculateAverage()}</div>
+      <div>positive {calculatePositivePercentage()}%</div>
       
     </div>
   )
