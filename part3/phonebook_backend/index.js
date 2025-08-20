be90a42e31c1d808c307e0cc1317feae15f75e8b
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-08-20 17:34:53 +0200

    Implement phonebook backend step 6

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -4,26 +4,26 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    "id": "1",
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": "2",
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": "3",
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": "4",
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
 ]
 
 
@@ -55,21 +55,38 @@ app.get('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  if (persons.some(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
   const id = String(Math.floor(Math.random() * (6000 - 1000) + 1000))
 
-  const person = {id: id, ...body}
+  const person = { id: id, ...body }
 
   persons = persons.concat(person)
 
   response.json(person)
-  
+
 })
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
 
   persons = persons.filter(person => person.id !== id)
-  
+
   response.status(204).end()
 })
 
