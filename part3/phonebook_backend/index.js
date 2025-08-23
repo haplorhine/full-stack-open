65a9c1aaa1cad91c2b232667e68881da4136e71e
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-08-23 17:10:58 +0200

    uibuild

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -26,6 +26,7 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
