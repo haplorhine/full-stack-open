ac56c4473eefb27e91a2da6be164ba3909c30e97
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-10-03 12:45:42 +0200

    Remove unused code

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -4,28 +4,6 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-// let persons = [
-//   {
-//     "id": "1",
-//     "name": "Arto Hellas",
-//     "number": "040-123456"
-//   },
-//   {
-//     "id": "2",
-//     "name": "Ada Lovelace",
-//     "number": "39-44-5323523"
-//   },
-//   {
-//     "id": "3",
-//     "name": "Dan Abramov",
-//     "number": "12-43-234345"
-//   },
-//   {
-//     "id": "4",
-//     "name": "Mary Poppendieck",
-//     "number": "39-23-6423122"
-//   }
-// ]
 
 app.use(express.json())
 app.use(express.static('dist'))
@@ -36,12 +14,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.get('/', (request, response) => {
   response.send('<h1>Hello Phonebook!</h1>')
 })
-
-// app.get('/info', (request, response) => {
-//   const message = `Phonebook has info for ${persons.length} people`
-//   const dateOfRequest = Date()
-//   response.send(`<p>${message}</p><p>${dateOfRequest}</p>`)
-// })
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(result => {
