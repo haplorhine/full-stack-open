4d9b2d565ab83ca7faa0acfeac0ce8811ef14935
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2026-02-10 01:31:46 +0100

    remove comments

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,13 +2,6 @@ const dummy = (blogs) => {
   return 1
 }
 
-// Define a new totalLikes function that receives a list of blog posts as
-// a parameter. The function returns the total sum of likes
-// in all of the blog posts.
-
-// Write appropriate tests for the function. It's recommended to put the tests
-// inside of a describe block so that the test report output gets grouped nicely:
-
 const totalLikes = (blogs) => {
   let likes = 0
   blogs.forEach((element) => {
