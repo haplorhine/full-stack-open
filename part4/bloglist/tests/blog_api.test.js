c92e5accf91bdb2696b1d77453b99e27ea05221e
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2026-02-16 16:36:47 +0100

    Implement 4.9: Blog List Tests, step 2

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -6,13 +6,19 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test.only('all blogs are returned in json format', async () => {
+test('all blogs are returned in json format', async () => {
   const response = await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 
   assert.strictEqual(response.body.length, 2)
+})
+
+test.only('unique identifier property of the blog posts is named "id"', async () => {
+  const response = await api.get('/api/blogs')
+  const blogs = response.body
+  assert(blogs.every((blog) => Object.hasOwn(blog, 'id')))
 })
 
 after(async () => {
