8982c7146bcc7379bf35b0540439532c3a140345
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2026-02-16 15:54:53 +0100

    Implement 4.8: Blog List Tests, step 1

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,20 @@
+const { test, after } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test.only('all blogs are returned in json format', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.length, 2)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
