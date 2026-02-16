a39fe82f8e1b5cd08866d9ac94d99bb6e54c3cd8
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2026-02-16 18:47:29 +0100

    Rename parameter

--- part4/bloglist/tests/test_helper.js
+++ part4/bloglist/tests/test_helper.js
@@ -21,7 +21,7 @@ const initialBlogs = [
 
 const blogsInDb = async () => {
   const blogs = await Blog.find({})
-  return blogs.map((note) => note.toJSON())
+  return blogs.map((blog) => blog.toJSON())
 }
 
 module.exports = {
