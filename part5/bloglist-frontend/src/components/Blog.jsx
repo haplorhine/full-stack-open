b8c76f6e958b9788b57b7d4b4f9bc14df6d4cb6d
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2026-03-04 20:41:02 +0100

    Implement 5.7 Blog List Frontend, step 7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    padding: '.5em',
+    border: '1px solid',
+    borderRadius: '6px',
+    background: 'aliceblue',
+    marginBottom: '10px',
+  }
+
+  const [detailsShown, setDetailsShown] = useState(false)
+
+  const toggleDisplay = () => {
+    setDetailsShown(!detailsShown)
+  }
+
+  const blogDetails = () => (
+    <>
+      <div>{blog.url}</div>
+      <div>
+        {blog.likes} <button>like</button>
+      </div>
+      <div>{blog.user?.name}</div>
+    </>
+  )
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{' '}
+      <button onClick={toggleDisplay}>{detailsShown ? 'hide' : 'view'}</button>
+      {detailsShown && blogDetails()}
+    </div>
+  )
+}
 
 export default Blog
