683a54609ae510f447ba2a718ad8feac6e572a03
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2026-03-04 20:33:28 +0100

    Populate user on post

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,59 +1,76 @@
-const jwt = require('jsonwebtoken')
-
-const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
+const blogsRouter = require('express').Router()
+const { userExtractor } = require('../utils/middleware')
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', { blogs: 0 })
-  response.json(blogs)
+blogsRouter.get('/', (request, response) => {
+  Blog.find({})
+    .populate('user', { username: 1, name: 1, id: 1 })
+    .then((blogs) => {
+      response.json(blogs)
+    })
 })
 
-blogsRouter.post('/', async (request, response) => {
-  const body = request.body
+blogsRouter.post('/', userExtractor, async (request, response) => {
+  const user = request.user
+  const blog = new Blog(request.body)
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
+  blog.likes = blog.likes | 0
+  blog.user = user._id
 
-  if (!user) {
-    return response.status(400).json({ error: 'userId missing or not valid' })
+  if (!blog.title || !blog.url) {
+    return response.status(400).send({ error: 'title or url missing' })
   }
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0,
-    user: user._id,
-  })
-
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog)
+  user.blogs = user.blogs.concat(blog._id)
   await user.save()
 
-  response.status(201).json(savedBlog)
+  const savedBlog = await blog.save()
+
+  const populatedBlog = await savedBlog.populate('user', {
+    username: 1,
+    name: 1,
+    id: 1,
+  })
+  console.log('populated blog', populatedBlog)
+
+  response.status(201).json(populatedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
+  const user = request.user
+  const blog = await Blog.findById(request.params.id)
+
+  if (!blog) {
+    return response.status(204).end()
+  }
+
+  if (user.id.toString() !== blog.user.toString()) {
+    return response.status(403).json({ error: 'user not authorized' })
+  }
+
+  user.blogs = user.blogs.filter((b) => b.id.toString() !== blog.id.toString())
+
+  await blog.deleteOne()
   response.status(204).end()
 })
 
 blogsRouter.put('/:id', async (request, response) => {
-  const { likes } = request.body
+  const { title, author, url, likes } = request.body
 
   const blog = await Blog.findById(request.params.id)
+
   if (!blog) {
     return response.status(404).end()
   }
 
+  blog.title = title
+  blog.author = author
+  blog.url = url
   blog.likes = likes
 
-  await blog.save()
-  response.json(blog)
+  const updatedBlog = await blog.save()
+
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
