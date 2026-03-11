0369b57591fc26d4ff381ad56f861d37d559218c
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2026-03-11 19:27:08 +0100

    Implement 5.14: Blog List Tests, step 2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title', () => {
@@ -60,4 +61,38 @@ test('does not render likes by default', () => {
 
   const div = container.querySelector('.likes')
   expect(div).toBeNull()
+})
+
+test('shows likes when details button is clicked', async () => {
+  const blog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 420,
+  }
+
+  render(<Blog blog={blog} />)
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  screen.getByText('420')
+})
+
+test('shows url when details button is clicked', async () => {
+  const blog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 420,
+  }
+
+  render(<Blog blog={blog} />)
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  screen.getByText(
+    'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+  )
 })
