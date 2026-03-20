32ef693a3ccc0b66f970982d105ac7cf050fefbd
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2026-03-20 15:04:23 +0100

    Implement 6.3: Anecdotes, step 1

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,16 +1,19 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { voteFor } from './reducers/anecdoteReducer'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector((state) => state)
+  const dispatch = useDispatch()
 
-  const vote = id => {
+  const vote = (id) => {
     console.log('vote', id)
+    dispatch(voteFor(id))
   }
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote => (
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
