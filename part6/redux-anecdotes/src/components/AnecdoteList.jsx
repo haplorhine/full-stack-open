7d450976e20d0bfba43ac48639d8a944c5562641
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2026-03-21 18:48:28 +0100

    Implement 6.9 Anecdotes, step 7

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,12 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteFor } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state)
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    return anecdotes.filter((anecdote) =>
+      anecdote.content.toLowerCase().includes(filter.toLowerCase()),
+    )
+  })
+
   const dispatch = useDispatch()
 
   const vote = (id) => {
