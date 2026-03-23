306100a3a031ded52f3e5e0f668b7f6b721c1070
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2026-03-23 14:30:33 +0100

    Implement 6.15 Anecdotes and the Backend, step 2

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -4,16 +4,19 @@ import {
   removeNotification,
   setNotification,
 } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newAnecdote))
     dispatch(setNotification(`You created a new anecdote: '${content}'`))
+
     setTimeout(() => {
       dispatch(removeNotification())
     }, 5000)
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -26,12 +26,12 @@ const anecdoteSlice = createSlice({
   initialState,
   reducers: {
     createAnecdote(state, action) {
-      const content = action.payload
-      state.push(asObject(content))
+      state.push(action.payload)
     },
     voteFor(state, action) {
       const id = action.payload
       const anecdoteToChange = state.find((n) => n.id === id)
+      console.log('anecdoteto change', current(anecdoteToChange))
       const votedForAnecdote = {
         ...anecdoteToChange,
         votes: anecdoteToChange.votes + 1,
