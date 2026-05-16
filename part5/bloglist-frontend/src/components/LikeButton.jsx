72710f08e68a69f011231448f4b37063bb2ca9b0
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2026-05-16 17:50:48 +0200

    Remove part5, part6, part7 to redo

--- part5/bloglist-frontend/src/components/LikeButton.jsx
+++
@@ -1,5 +0,0 @@
-const LikeButton = ({ onLike }) => {
-  return <button onClick={onLike}>like</button>
-}
-
-export default LikeButton
--- part5/bloglist-frontend/src/components/LoggedInUser.jsx
+++
@@ -1,7 +0,0 @@
-const LoggedInUser = ({ user, onLogout }) => (
-  <p>
-    {user.name} logged in <button onClick={onLogout}>logout</button>
-  </p>
-)
-
-export default LoggedInUser
